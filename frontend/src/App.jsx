00b98ddb5c7389c5fc4af156269be007e8b01889
Hamza Mehmood <thehamzamehmood@gmail.com>
2024-11-05 12:55:25 +0500

    Added Pages and Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './pages/Home'
 import './App.css'
+
+const routes=createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  }
+])
 
 function App() {
  
-
-  return (
-    <>
-    <div className='text-purple-400'>Hello</div>
-   
-    </>
-  )
+  return <div> <RouterProvider router={routes}></RouterProvider></div>   
 }
 
 export default App
