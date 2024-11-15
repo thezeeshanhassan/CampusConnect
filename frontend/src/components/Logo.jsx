37d7e516f603b6998be304d6fca3e4d307049358
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-11-15 16:00:40 +0500

    Header Some More Completed

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 export default function Logo() {
     return (
         <>
-            <div className="container flex h-16 items-center justify-between px-4">
-                <Link to={"/"} className="flex items-center space-x-2">
-                    <span className="text-4xl font-bold">
-                        <span className="text-blue-600">S</span>
-                        <span className="text-red-500">k</span>
-                        <span className="text-orange-400">o</span>
-                        <span className="text-blue-600">o</span>
-                        <span className="text-gray-800">l</span>
-                    </span>
-                </Link>
-            </div>
+            <Link to={"/"} className="flex items-center space-x-2">
+                <span className="text-4xl font-bold">
+                    <span className="text-blue-600">S</span>
+                    <span className="text-red-500">k</span>
+                    <span className="text-orange-400">o</span>
+                    <span className="text-blue-600">o</span>
+                    <span className="text-gray-800">l</span>
+                </span>
+            </Link>
         </>
     );
 }
