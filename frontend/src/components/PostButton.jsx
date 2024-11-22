50423317fb8e4ff9e566faf08cefea4c05f0db0f
Hamza Mehmood <thehamzamehmood@gmail.com>
2024-11-22 23:37:45 +0500

    ComunityPage/Posts

--- frontend/src/components/PostButton.jsx
+++ frontend/src/components/PostButton.jsx
@@ -1,14 +1,28 @@
 import WritePostModal from './WritePostModal';
 import { useState } from 'react';
+import gamingPic from '../assets/gaming1.jpg'
 
 export default function PostButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
-      <button onClick={() => setIsModalOpen(true)}>
-        Create Post
-      </button>
+      <div onClick={() => setIsModalOpen(true)} className= " container bg-white shadow-md rounded-lg p-4 border-x-slate-400">
+          <div className="flex gap-4 justify-center items-center ">
+        
+            <div className="w-10 h-10 rounded-full overflow-hidden bg-gray-200 flex items-center justify-center">
+              <img
+                src={gamingPic}
+                alt="User"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className='flex-1 font-semibold text-xl text-gray-500'>
+                Write something
+            </div>
+          </div>
+        </div>
+
       
       <WritePostModal
         isOpen={isModalOpen}
