f28a7b85d0f7db9151edee39b4daa2f94913d86f
Hamza Mehmood <thehamzamehmood@gmail.com>
2024-12-02 23:07:25 +0500

    Adding Sample Files

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
-const express= require('express');
+const express = require("express");
 
 const app = express();  // express 
-
+const userRouter = require('./routes/user');
 
 app.get('/', function(req, res) {
     res.send('Hello, World!');
 });
 
+
+
+app.use('/api/v1/user', userRouter); 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');});
