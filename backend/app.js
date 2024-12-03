830a6a7c3458f360ff87596ba789352184f06952
Hamza Mehmood <thehamzamehmood@gmail.com>
2024-12-03 23:49:23 +0500

    Finalizing Initializing

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require(`passport-local`); // it is local stretgy to  login user
 require('dotenv').config();
-
-const app = express();  // express 
 const userRouter = require('./routes/user');
 const connectDB = require("./db");
-
+const app = express();
 
 connectDB();
-app.get('/', function(req, res) {
+
+//Middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize()); //It initialize the passport lib
+app.use(passport.session()); //it starts the session so same user can to different tabs
+
+//Routes
+app.get('/', function (req, res) {
     res.send('Hello, World!');
 });
+app.use('/api/v1/user', userRouter);
 
 
 
-app.use('/api/v1/user', userRouter); 
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');});
+
+// Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+app.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
