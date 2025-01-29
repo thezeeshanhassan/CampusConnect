970c75c50111846dd33d0fd9d5a02ccd7fafd211
HamzaMehmood <thehamzamehmood@gmail.com>
2025-01-29 16:52:25 +0500

    final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,69 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './pages/Login';
-import HomeLayout from './layout/RootLayout';
-import Home from './pages/Home';
-import SignupPage from './pages/SignupPage';
-import AboutComunityPage from './pages/AboutComunityPage';
-import CommunityPage from './pages/CommunityPage';
-import CommunityLayout from './layout/CommunityLayout';
-import MembersCommunityPage from './pages/MembersCommunityPage';
-import './App.css'
-import CreateCommunity from './pages/CreateCommunity';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import HomeLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+import SignupPage from "./pages/SignupPage";
+import AboutComunityPage from "./pages/AboutComunityPage";
+import CommunityPage from "./pages/CommunityPage";
+import CommunityLayout from "./layout/CommunityLayout";
+import MembersCommunityPage from "./pages/MembersCommunityPage";
+import "./App.css";
+import CreateCommunity from "./pages/CreateCommunity";
 
 const routes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     children: [
       {
-
         index: true,
-        element: <Home/>
+        element: <Home />,
       },
       {
-        path: '/about',
-        element: <AboutComunityPage/>
-      }
-    ]
+        path: "/about",
+        element: <AboutComunityPage />,
+      },
+    ],
   },
   {
-    path: '/create-community',
-    element: <CreateCommunity/>
+    path: "/create-community",
+    element: <CreateCommunity />,
   },
   {
-    path: '/login',
-    element: <Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/signup',
-    element: <SignupPage/>
+    path: "/signup",
+    element: <SignupPage />,
   },
   {
-    path: '/community',
-    element: <CommunityLayout/>,
+    path: "/community",
+    element: <CommunityLayout />,
     children: [
       {
         index: true,
-        element: <CommunityPage/>
+        element: <CommunityPage />,
       },
       {
-        path: 'members',
-        element: <MembersCommunityPage/>
+        path: "members",
+        element: <MembersCommunityPage />,
       },
       {
-        path: 'about',
-        element:<AboutComunityPage/>
-      }
-    ]
-  }
-])
+        path: "about",
+        element: <AboutComunityPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return <div> <RouterProvider router={routes}></RouterProvider></div>
+  return (
+    <div>
+      {" "}
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
